fcdcb05f8362014928d3d5a45ad3a6c02b4ac5d5
TomasRomanoo <romano.tomas@outlook.com.ar>
2022-03-30 11:54:44 -0300

    +orden +productos

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,17 +1,17 @@
 // import { Navbar } from 'react-bootstrap';
-import NavBar from './components/navBar/NavBar'
+import NavBar from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import logo from './logo.svg';
 import './App.css';
-import ItemCount from './components/contador/ItemCount';
-
+import ItemListContainer from './components/itemListContainer';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemCount />
+      <ItemListContainer />
+      
     </div>
   );
 }
